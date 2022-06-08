60c0e238c7814effad1d5ac0a417c7d4ecccceed
Rodrigo <210100299@esg.ipsantarem.pt>
2022-06-08 10:44:27 +0100

    #3 - Add JUnit tests to CalculatorAPI.java

---
+++ ex3/src/test/java/pt/brunojesus/esg/CalculatorAPITest.java
@@ -0,0 +1,34 @@
+package pt.brunojesus.esg;
+
+import org.junit.jupiter.api.Test;
+import pt.brunojesus.esg.operations.Operation;
+import pt.brunojesus.esg.operations.impl.DivisionOperation;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static sun.jvm.hotspot.oops.ConstantPool.operands;
+
+class CalculatorAPITest {
+
+
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide()
+    {
+
+    }
+
+}
